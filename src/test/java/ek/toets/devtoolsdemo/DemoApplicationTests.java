1acc05d92fbfdb56f36dcd232162e943f5e7f9a0
Anthony van Deventer <anthony.vandeventer@standardbank.co.za>
2021-09-09 09:54:30 +0200

    this is a test for intellij

---
+++ src/test/java/ek/toets/devtoolsdemo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ek.toets.devtoolsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
